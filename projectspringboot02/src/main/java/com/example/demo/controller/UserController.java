ace1e53a26a75a462e7a87584af7212d6d67e155
zwl <1831622781@qq.com>
2022-11-09 16:30:37 +0800

    20221109b

--- projectspringboot02/src/main/java/com/example/demo/controller/UserController.java
+++ projectspringboot02/src/main/java/com/example/demo/controller/UserController.java
@@ -1,16 +1,51 @@
 package com.example.demo.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.common.Result;
+import com.example.demo.mapper.UserMapper;
 import com.example.demo.pojo.User;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 @RequestMapping("/user")
 @RestController
 public class UserController {
 
+    @Resource
+    UserMapper userMapper;
+
     @PostMapping
-    public Result save(@RequestBody User user){
-        return null;
+    public Result<?> save(@RequestBody User user){
+        userMapper.insert(user);
+        return Result.success();
+
+    }
+
+    @GetMapping
+    public Result<?> findPage(@RequestParam(defaultValue = "1") Integer pageNum,
+                              @RequestParam(defaultValue = "10") Integer pageSize,
+                              @RequestParam(defaultValue = "") String search){
+        LambdaQueryWrapper<User> wrapper = Wrappers.<User>lambdaQuery();
+        if (StringUtils.isNotBlank(search)){
+            wrapper.like(User::getUserName,search);
+        }
+
+        Page<User> userPage = userMapper.selectPage(new Page<>(pageNum, pageSize),
+                wrapper);
+
+        return Result.success(userPage);
+
+
+    }
+
+    @PutMapping
+    public Result<?> update(@RequestBody User user){
+        userMapper.updateById(user);
+        return Result.success();
 
     }
 
--- projectvue02/src/views/Home.vue
+++ projectvue02/src/views/Home.vue
@@ -2,45 +2,71 @@
   <div class="home" style="padding: 10px">
 <!--    功能区-->
     <div style="margin: 10px 0">
-      <el-button type="primary">新增</el-button>
+      <el-button type="primary" @click="add()">新增</el-button>
       <el-button type="primary">导入</el-button>
       <el-button type="primary">导出</el-button>
     </div>
 <!--    搜索区-->
     <div style="margin: 10px 0;">
-      <el-input v-model="search" placeholder="请输入关键字" style="width: 20%"></el-input>
-      <el-button type="primary" style="margin-left: 5px">查询</el-button>
+      <el-input v-model="search" placeholder="请输入关键字" style="width: 20%" clearable></el-input>
+      <el-button type="primary" style="margin-left: 5px" @click="load" >查询</el-button>
     </div>
+<!--    表格区-->
     <el-table :data="tableData" border stripe style="width: 100%;display: flex;">
-      <el-table-column prop="date" label="日期" sortable  />
-      <el-table-column prop="name" label="姓名" sortable  />
+      <el-table-column prop="userId" label="ID" sortable  />
+      <el-table-column prop="userName" label="用户名" sortable  />
+      <el-table-column prop="password" label="密码" />
       <el-table-column prop="address" label="地址" />
       <el-table-column fixed="right" label="操作" width="180">
-        <template #default>
-          <el-button @click="handleEdit" >编辑</el-button>
-          <el-popconfirm title="确认删除吗?">
+        <template #default="scope">
+          <el-button @click="handleEdit(scope.row)" >编辑</el-button>
+          <el-popconfirm title="确认删除吗?" @confirm="handleDelete(scope.row.id)">
             <template #reference>
-              <el-button type="danger" >删除</el-button>
+              <el-button type="danger">删除</el-button>
             </template>
           </el-popconfirm>
         </template>
       </el-table-column>
     </el-table>
+<!--    分页栏-->
     <div style="margin: 10px 0">
       <el-pagination
           v-model:currentPage="currentPage"
           v-model:page-size="pageSize"
           :page-sizes="[5,10,20]"
-          :small="small"
-          :disabled="disabled"
-          :background="background"
           layout="total, sizes, prev, pager, next, jumper"
-          :total="10"
+          :total="total"
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
       />
 
     </div>
+<!--    新增弹窗-->
+    <el-dialog
+        v-model="dialogVisible"
+        title="提示"
+        width="30%"
+    >
+      <el-form  :model="form" label-width="120px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.userName" style="width: 80%" />
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="form.password" style="width: 80%"/>
+        </el-form-item>
+        <el-form-item label="地址">
+          <el-input type="textarea"  v-model="form.address" style="width: 80%"/>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">返回</el-button>
+        <el-button type="primary" @click="save" click="dialogVisible = false">
+          确定
+        </el-button>
+      </span>
+      </template>
+    </el-dialog>
 
   </div>
 </template>
@@ -48,44 +74,101 @@
 <script>
 // @ is an alias to /src
 
+import request from "@/utils/request";
+
 export default {
-  name: 'HomeView',
+
+  name: 'Home',
+
   components: {
 
   },
+  created() {
+    this.load()
+  },
   methods:{
-    handleEdit(){
+    handleDelete(){
 
     },
+    handleEdit(row){
+      this.form = JSON.parse(JSON.stringify(row))
+      this.dialogVisible=true
+
+    },
+    add(){
+      this.dialogVisible =true
+      this.form = {}
+    },
+
+    save(){
+      if (this.form.userId){
+        request.put("/api/user",this.form).then(res =>{
+          console.log(res)
+          if (res.code==='0'){
+            this.$message({
+              type:"success",
+              message:"更新成功"
+            })
+          }else {
+            this.$message({
+              type:"error",
+              message:res.msg
+            })
+          }
+
+
+
+        })
+
+
+      }else{
+        request.post("/api/user",this.form).then(res =>{
+          console.log(res)
+          this.$message({
+            type:"success",
+            message:"新增成功"
+          })
+
+
+        })
+      }
+      this.load()
+      this.dialogVisible=false
+    },
+    load(){
+      request.get("/api/user",{
+        params:{
+          pageNum:this.currentPage,
+          pageSize:this.pageSize,
+          search:this.search
+        }
+      }).then(res=>{
+        console.log(res)
+        this.tableData = res.data.records
+        this.total = res.data.total
+      })
+    },
+    handleSizeChange(pageSize){
+      this.pageSize = pageSize
+      this.load()
+    },
+    handleCurrentChange(pageNum){
+      this.currentPage=pageNum
+      this.load()
+    }
 
 
   },
   data(){
     return {
       search:'',
+      dialogVisible: false,
+      form:{},
+      currentPage:1,
+      pageSize:10,
+      total:0,
 
-      tableData:[
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-      ]
+      tableData:[],
     }
   }
 }
